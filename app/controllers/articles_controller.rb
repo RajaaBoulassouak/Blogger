34c33cecdafe4b2dafea4be76b84ef5288b53072
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-08-24 21:36:33 -0600

    Add flash meassage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,14 +15,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
+    flash.notice = "Article #{@article.title} Created!"
     redirect_to article_path(@article)
   end
   
   def destroy 
     @article.destroy
-    
     redirect_to articles_path
+    flash.notice = "Article #{@article.title} Deleted!"
   end
   
   def edit
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
   
   def update
     @article.update(article_params)
-
+    flash.notice = "Article #{@article.title} Updated!"
     redirect_to article_path(@article)
   end
   
